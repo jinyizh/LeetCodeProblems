a0ca628b72c1fc499c05d74426f8aefc67e76f03
Jinyi Zhou <jinyi.zhou@utah.edu>
2022-11-24 00:40:00 -0700

    lc3

---
+++ LeetCodeProblems/src/lc3_longest_substring_without_repeating_characters/Main.java
@@ -0,0 +1,22 @@
+package lc3_longest_substring_without_repeating_characters;
+
+/**
+ * Given a string s, find the length of the longest substring without repeating characters.
+ * ---
+ * Example 1:
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc".
+ * ---
+ * Example 2:
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke".
+ * ---
+ * Constraints:
+ * 0 <= s.length <= 5 * 10^4
+ * s consists of English letters, digits, symbols and spaces.
+ */
+
+public class Main {
+}
